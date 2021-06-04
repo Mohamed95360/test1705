17b21119de25158f5caecee479cddc7f341efad4
mohamed soilihi <msoilihi122@gmail.com>
2021-06-04 15:45:39 +0200

    modif fichier App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Bonjour c'est moi
+        </p>
+
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
